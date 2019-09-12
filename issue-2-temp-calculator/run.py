e4a23a8035ed74f4087f26cd2bf3e66c87e6ee4b
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-12 22:36:25 +0200

    string in input

--- issue-2-temp-calculator/run.py
+++ issue-2-temp-calculator/run.py
@@ -1,25 +1,22 @@
 textCtoF = "1) Stopnie Celsjusza na Fahrenheita"
 textFtoC = "2) Stopnie Fahrenheita na Celsjusza"
-order = "Podaj wartość:"
+order = "Podaj wartość: "
 
 print("Program do konwersji temperatury")
 print("Jaką konwersje chcesz wykonać?")
 print(textCtoF)
 print(textFtoC)
-print("Wybierz:")
 
-choice = int(input())
+choice = int(input("Wybierz: "))
 
 if choice == 1:
     print(textCtoF[3:])
-    print(order)
-    number1= float(input())
+    number1= float(input(order))
     result1 = number1* 1.8 + 32
     print(number1, "°C to", result1, "°F")
 
 if choice == 2:
     print(textFtoC[3:])
-    print(order)
-    number2= float(input())
+    number2= float(input(order))
     result2 = (number2 - 32) / 1.8
     print(number2, "°F to", result2, "°C")
