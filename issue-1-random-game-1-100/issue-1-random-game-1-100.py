1e3c21b874b93c70324327f521e0913aecd1aa48
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-19 16:33:34 +0200

    while True

--- issue-1-random-game-1-100/issue-1-random-game-1-100.py
+++ issue-1-random-game-1-100/issue-1-random-game-1-100.py
@@ -5,12 +5,14 @@ shot = 101
 print("Została wylosowana liczba z przedziału 0-100.")
 text = "Zgadnij jaka to liczba: "
 
-while shot != randomNumber:
+while True:
     shot = int(input(text))
     text = "Zgadnij jeszcze raz: "
     if shot > randomNumber:
         print("Za duża liczba. ")
     if shot < randomNumber:
         print("Za mała liczba. ")
+    if shot == randomNumber:
+        break
 
 print("Tak! Brawo!")
