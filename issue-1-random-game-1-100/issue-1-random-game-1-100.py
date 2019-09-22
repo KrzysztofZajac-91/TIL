6293277cb66475f98c159c71faf8447498f2539b
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-22 19:41:29 +0200

    simpler loop

--- issue-1-random-game-1-100/issue-1-random-game-1-100.py
+++ issue-1-random-game-1-100/issue-1-random-game-1-100.py
@@ -1,18 +1,18 @@
 from random import randint
 randomNumber = randint(0, 100)
-shot = 101
-
 print("Została wylosowana liczba z przedziału 0-100.")
+
 text = "Zgadnij jaka to liczba: "
+shot = int(input(text))
+text = "Zgadnij jeszcze raz: "
 
 while True:
-    shot = int(input(text))
-    text = "Zgadnij jeszcze raz: "
     if shot > randomNumber:
         print("Za duża liczba. ")
     if shot < randomNumber:
         print("Za mała liczba. ")
     if shot == randomNumber:
         break
+    shot = int(input(text))
 
 print("Tak! Brawo!")
