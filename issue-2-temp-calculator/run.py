0b86ff2554721a99dd80519b4510819e4e63ad54
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-24 15:52:50 +0200

    delete two prints in if

--- issue-2-temp-calculator/run.py
+++ issue-2-temp-calculator/run.py
@@ -1,22 +1,18 @@
-textCtoF = "1) Stopnie Celsjusza na Fahrenheita"
-textFtoC = "2) Stopnie Fahrenheita na Celsjusza"
 order = "Podaj wartość: "
 
 print("Program do konwersji temperatury")
 print("Jaką konwersje chcesz wykonać?")
-print(textCtoF)
-print(textFtoC)
+print("1) Stopnie Celsjusza na Fahrenheita")
+print("2) Stopnie Fahrenheita na Celsjusza")
 
 choice = int(input("Wybierz: "))
 
 if choice == 1:
-    print(textCtoF[3:])
     number1 = float(input(order))
     result1 = number1 * 1.8 + 32
     print("{}°C to {}°F".format(number1, result1))
 
 if choice == 2:
-    print(textFtoC[3:])
     number2 = float(input(order))
     result2 = (number2 - 32) / 1.8
     print("{}°F to {}°C".format(number2, result2))
