5dc5272fb7f3fde1f22bce55c77eb4c0b2713bcf
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-24 16:51:34 +0200

    input before if, rename variables, add elif

--- issue-2-temp-calculator/run.py
+++ issue-2-temp-calculator/run.py
@@ -1,18 +1,14 @@
-order = "Podaj wartość: "
-
 print("Program do konwersji temperatury")
 print("Jaką konwersje chcesz wykonać?")
 print("1) Stopnie Celsjusza na Fahrenheita")
 print("2) Stopnie Fahrenheita na Celsjusza")
 
 choice = int(input("Wybierz: "))
+inputTemperature = float(input("Podaj wartość: "))
 
 if choice == 1:
-    number1 = float(input(order))
-    result1 = number1 * 1.8 + 32
-    print("{}°C to {}°F".format(number1, result1))
-
-if choice == 2:
-    number2 = float(input(order))
-    result2 = (number2 - 32) / 1.8
-    print("{}°F to {}°C".format(number2, result2))
+    conversionResult = inputTemperature * 1.8 + 32
+    print("{}°C to {}°F".format(inputTemperature, conversionResult))
+elif choice == 2:
+    conversionResult = (inputTemperature - 32) / 1.8
+    print("{}°F to {}°C".format(inputTemperature, conversionResult))
