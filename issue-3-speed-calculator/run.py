bdc8f4b85c561e16ed012a4b6881902759264bae
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-25 17:19:12 +0200

    prints

---
+++ issue-3-speed-calculator/run.py
@@ -0,0 +1,9 @@
+print("Program do konwersji prędkości w różnych jednostkach.")
+
+
+def conversion_selection():
+    print("1) m/s na km/h")
+    print("2) km/h na m/s")
+    print("3) mile/h na km/h")
+    print("4) km/h na mile/h")
+    print("5) km/h na procent prędkości światła w próźni")
