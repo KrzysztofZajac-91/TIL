6064d883ae599a446324d7bbb0d679818fc1ea87
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-25 21:09:29 +0200

    rename def, add two conversion

--- issue-3-speed-calculator/run.py
+++ issue-3-speed-calculator/run.py
@@ -1,7 +1,7 @@
 print("Program do konwersji prędkości w różnych jednostkach.")
 
 
-def conversion_selection():
+def conversion_list():
     print("1) m/s na km/h")
     print("2) km/h na m/s")
     print("3) mile/h na km/h")
@@ -11,7 +11,17 @@ def conversion_selection():
 
 while True:
     print("Jaką konwersje chcesz wykonać?")
-    conversion_selection()
+    conversion_list()
+    conversion_choice = int(input("Wybierz: "))
+    input_speed = float(input("Podaj wartość: "))
+
+    if conversion_choice == 1:
+        conversion_result = input_speed * 3.6
+        print("{} m/s to {} km/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 2:
+        conversion_result = input_speed / 3.6
+        print("{} km/h to {} m/s".format(input_speed, conversion_result))
 
     closing_decision = input("Czy zakończyć program ([T]ak/[N]ie)? ")
     if closing_decision == "T" or closing_decision == "t" or closing_decision == "Tak" or closing_decision == "tak":
