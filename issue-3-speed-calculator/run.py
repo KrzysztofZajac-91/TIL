0666555f3d75b6fe7f3be126b628fbefbefc146b
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-09-25 23:08:32 +0200

    add three conversion

--- issue-3-speed-calculator/run.py
+++ issue-3-speed-calculator/run.py
@@ -23,6 +23,18 @@ while True:
         conversion_result = input_speed / 3.6
         print("{} km/h to {} m/s".format(input_speed, conversion_result))
 
+    elif conversion_choice == 3:
+        conversion_result = input_speed * 1.609344
+        print("{} mile/h to {} km/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 4:
+        conversion_result = input_speed / 1.609344
+        print("{} km/h to {} mile/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 5:
+        conversion_result = input_speed * 100 / 1079252849
+        print("{} km/h to {}% prędkości światła w próźni".format(input_speed, conversion_result))
+
     closing_decision = input("Czy zakończyć program ([T]ak/[N]ie)? ")
     if closing_decision == "T" or closing_decision == "t" or closing_decision == "Tak" or closing_decision == "tak":
         break
