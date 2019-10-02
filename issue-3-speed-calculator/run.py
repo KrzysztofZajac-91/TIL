2f7088c656349fe1c05665c4b2843c45e087127a
KrzysztofZajac-91 <54445127+KrzysztofZajac-91@users.noreply.github.com>
2019-10-03 00:21:10 +0200

    shorter try, add else

--- issue-3-speed-calculator/run.py
+++ issue-3-speed-calculator/run.py
@@ -15,39 +15,38 @@ while True:
 
     try:
         conversion_choice = int(input("Wybierz: "))
-
-        if conversion_choice > 5 or conversion_choice < 1:
-            print("Nieprawidłowa wartość")
-            continue
-
         input_speed = float(input("Podaj wartość: "))
-
-        if conversion_choice == 1:
-            conversion_result = input_speed * 3.6
-            print("{} m/s to {} km/h".format(input_speed, conversion_result))
-
-        elif conversion_choice == 2:
-            conversion_result = input_speed / 3.6
-            print("{} km/h to {} m/s".format(input_speed, conversion_result))
-
-        elif conversion_choice == 3:
-            conversion_result = input_speed * 1.609344
-            print("{} mile/h to {} km/h".format(input_speed, conversion_result))
-
-        elif conversion_choice == 4:
-            conversion_result = input_speed / 1.609344
-            print("{} km/h to {} mile/h".format(input_speed, conversion_result))
-
-        elif conversion_choice == 5:
-            conversion_result = input_speed * 100 / 1079252849
-            print("{} km/h to {}% prędkości światła w próźni".format(input_speed, conversion_result))
-
-        closing_decision = input("Czy zakończyć program ([T]ak/[N]ie)? ")
-        if closing_decision == "T" or closing_decision == "t" or closing_decision == "Tak" or closing_decision == "tak":
-            break
-
     except ValueError:
         print("Nieprawidłowa wartość")
-
+        continue
     except():
         print("Nieznany błąd")
+        continue
+
+    if conversion_choice == 1:
+        conversion_result = input_speed * 3.6
+        print("{} m/s to {} km/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 2:
+        conversion_result = input_speed / 3.6
+        print("{} km/h to {} m/s".format(input_speed, conversion_result))
+
+    elif conversion_choice == 3:
+        conversion_result = input_speed * 1.609344
+        print("{} mile/h to {} km/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 4:
+        conversion_result = input_speed / 1.609344
+        print("{} km/h to {} mile/h".format(input_speed, conversion_result))
+
+    elif conversion_choice == 5:
+        conversion_result = input_speed * 100 / 1079252849
+        print("{} km/h to {}% prędkości światła w próźni".format(input_speed, conversion_result))
+
+    else:
+        print("Nieprawidłowa wartość")
+        continue
+
+    closing_decision = input("Czy zakończyć program ([T]ak/[N]ie)? ")
+    if closing_decision == "T" or closing_decision == "t" or closing_decision == "Tak" or closing_decision == "tak":
+        break
